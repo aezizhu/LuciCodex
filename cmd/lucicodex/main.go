efc652bc46ef61a7e04dd9a69321a944b2a6fb62
Aezi <aezi.zhu@icloud.com>
2025-11-23 12:40:15 +0800

    Bump version to 0.4.1

--- cmd/lucicodex/main.go
+++ cmd/lucicodex/main.go
@@ -23,7 +23,7 @@ import (
 	"github.com/aezizhu/LuciCodex/internal/wizard"
 )
 
-const version = "0.4.0"
+const version = "0.4.1"
 
 func acquireLock() (*os.File, string, error) {
 	lockPaths := []string{"/var/lock/lucicodex.lock", "/tmp/lucicodex.lock"}
