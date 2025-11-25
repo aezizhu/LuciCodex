f239a66eee6691dc5e881746877b81d20a948b60
Aezi <aezi.zhu@icloud.com>
2025-11-25 14:30:10 +0800

    Update version constant to 0.4.23

--- cmd/lucicodex/main.go
+++ cmd/lucicodex/main.go
@@ -23,7 +23,7 @@ import (
 	"github.com/aezizhu/LuciCodex/internal/wizard"
 )
 
-const version = "0.4.22"
+const version = "0.4.23"
 
 func acquireLock() (*os.File, string, error) {
 	lockPaths := []string{"/var/lock/lucicodex.lock", "/tmp/lucicodex.lock"}
