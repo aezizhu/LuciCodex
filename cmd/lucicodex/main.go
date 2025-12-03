273e5daed3fc48cd82ceb7690e3be3f988c813f8
Aezi <aezi.zhu@icloud.com>
2025-12-03 11:22:37 +0800

    fix: overwrite home.htm with correct Lua syntax, bump to v0.4.49

--- cmd/lucicodex/main.go
+++ cmd/lucicodex/main.go
@@ -24,7 +24,7 @@ import (
 	"github.com/aezizhu/LuciCodex/internal/wizard"
 )
 
-var version = "0.4.48"
+var version = "0.4.49"
 
 var lockPaths = []string{"/var/lock/lucicodex.lock", "/tmp/lucicodex.lock"}
 
