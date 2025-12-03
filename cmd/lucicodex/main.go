15a0c885ff1135d934c38e6771cda0efee164b9a
Aezi <aezi.zhu@icloud.com>
2025-12-03 11:01:18 +0800

    fix: overwrite overview.htm with correct Lua syntax and bump to v0.4.47

--- cmd/lucicodex/main.go
+++ cmd/lucicodex/main.go
@@ -24,7 +24,7 @@ import (
 	"github.com/aezizhu/LuciCodex/internal/wizard"
 )
 
-var version = "0.4.46"
+var version = "0.4.47"
 
 var lockPaths = []string{"/var/lock/lucicodex.lock", "/tmp/lucicodex.lock"}
 
